805574830ff07dbd022a5915e038ec4252d96370
vladeiko <vladeiko999@gmail.com>
2022-07-25 15:48:01 +0300

    Add useAsyncData hook to, add Joke component

--- demo/ui-kit/src/ContentPageContext.ts
+++ demo/ui-kit/src/ContentPageContext.ts
@@ -2,6 +2,12 @@ export type UseState = <State>(initialState: State) => {
   value: State;
 };
 
+export type AsyncDataHook = <Data, Error = any>(
+  key: string,
+  fetcher: () => Promise<Data>
+) => { data: { value: Data }; error: { value: Error } };
+
 export interface ContentPageContext {
   useState: UseState;
+  useAsyncData: AsyncDataHook;
 }
--- demo/ui-kit/src/index.ts
+++ demo/ui-kit/src/index.ts
@@ -1,5 +1,6 @@
+export type { ContentPageContext, UseState } from './ContentPageContext';
+export { Counter } from './Counter';
+export { Joke } from './Joke';
+export { PrimaryButton } from './PrimaryButton';
 export { TestPage } from './TestPage';
 export { TextBlock } from './TextBlock';
-export { PrimaryButton } from './PrimaryButton';
-export { Counter } from './Counter';
-export type { ContentPageContext, UseState } from './ContentPageContext';
