6b3ceeb2f11a5ff6fc07cf130723f93cd25f336b
vladeiko <vladeiko999@gmail.com>
2022-11-25 18:16:26 +0300

    Fix setup function for preact

--- uni-jsx/src/setup.preact.ts
+++ uni-jsx/src/setup.preact.ts
@@ -1,10 +1,15 @@
-import { h } from 'preact';
-import * as hooks from 'preact/hooks';
-import { setup } from './setup';
 import { setupHooks } from './hooks/index';
+import { setup } from './setup';
+import { h } from 'preact';
+// @ts-ignore
+import * as React from 'react';
 
 (globalThis as any).__UNI_REACT__ = true;
 
-setup(h);
+setup((type: any, rawProps: any) => {
+  const { children, ...props } = rawProps;
 
-setupHooks(hooks);
+  return h(type, props, children);
+});
+
+setupHooks(React);
