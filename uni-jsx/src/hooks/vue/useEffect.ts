47ebdeb81f18da2102f79e77251ba689a4eea939
vladeiko <vladeiko999@gmail.com>
2022-12-07 09:46:30 +0300

    Add SSR check in Vue useEffect hook

--- uni-jsx/src/hooks/vue/useEffect.ts
+++ uni-jsx/src/hooks/vue/useEffect.ts
@@ -2,6 +2,7 @@
 import { watchPostEffect } from 'vue';
 import { areDepsEqual } from './areDepsEqual';
 import { getCurrentHook } from './getCurrentHook';
+import { isSSR } from './isSSR';
 
 export const useEffect = updateEffectImp();
 export const useLayoutEffect = updateEffectImp(true);
@@ -17,6 +18,10 @@ interface EffectState<Deps extends any[]> {
 function updateEffectImp(isLayout = false) {
   return <Deps extends any[]>(effect: Effect, deps: Deps): void => {
     const [hook] = getCurrentHook<EffectState<Deps>>();
+
+    if (isSSR) {
+      return;
+    }
 
     const task = () => {
       if (hook.state && areDepsEqual(deps, hook.state.deps)) {
